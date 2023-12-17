4d2e4be91f3d5da58bf1d3160ae422b202d208e8
bertoramos <ramossanchezalberto@gmail.com>
2023-12-17 22:12:49 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'portfolio_react',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
